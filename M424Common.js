045eab6c136d21cd2a232bc1a2531f20d39fb77a
M424 <mys40651147@gmail.com>
2022-08-13 02:43:20 +0900

    Array対応版min/max追加.

--- M424Common.js
+++ M424Common.js
@@ -1,30 +1,106 @@
 // ==UserScript==
 // @name         M424Common
-// @version      1.0
+// @version      1.1
 // @description  commonクラス
 // @author       M424
 // ==/UserScript==
 
 
-// 基底クラス
+/**
+ * @class 基底クラス
+ */
 class M424Base {
+    /**
+     * デバッグ出力を行うかのフラグ
+     * @type {Boolean}
+     */
     #isDebug;
-    constructor(isDebugMode = false) {
+
+    /**
+     * スクリプトID
+     * @type {string}
+     */
+    #scriptId;
+
+    /**
+     * @constructor
+     * @param {string} scriptId 
+     * @param {Boolean} isDebugMode デバック出力するか
+     */
+    constructor(scriptId, isDebugMode = false) {
+        this.#scriptId = scriptId;
         this.#isDebug = isDebugMode;
     }
+
+    /**
+     * infoログ出力
+     * @param  {...any} msg 出力メッセージ
+     */
     log(...msg) {
-        console.log(`[${SCRIPTID}]`, ...msg);
+        console.log(`[${this.#scriptId}]`, ...msg);
     }
+
+    /**
+     * デバッグログ出力
+     * @param  {...any} msg 出力メッセージ
+     */
     debug(...msg) {
         if( this.#isDebug ) { this.log(...msg); }
     }
 }
 
 /**
- * Elementの属性を一括登録する
- * @param {*} obj 
+ * 独自定義のメソッド群
  */
-Element.prototype.setAttributes = (obj) => {
+const M424 = {
+    Math: {
+        /**
+         * Array対応版 Math.max
+         * @param  {...Number} args Number配列
+         * @returns {Number} 引数内で一番大きなNumber. 
+         *                   引数が空の場合null. Number以外の場合NaN.
+         */
+        max: (...args) => {
+            let arr = [];
+            args.forEach( e => {
+                if( Array.isArray(e) ) {
+                    arr.push( ...e );
+                } else {
+                    arr.push( e );
+                }
+            });
+            const ret = Math.max.apply(null, arr);
+            if( ret === -Infinity ) return null;
+            return ret;
+        },
+
+        /**
+         * Array対応版 Math.min
+         * @param  {...Number} args Number配列
+         * @returns {Number} 引数内で一番小さなNumber. 
+         *                   引数が空の場合null. Number以外の場合NaN.
+         */
+         min: (...args) => {
+            let arr = [];
+            args.forEach( e => {
+                if( Array.isArray(e) ) {
+                    arr.push( ...e );
+                } else {
+                    arr.push( e );
+                }
+            });
+            const ret = Math.min.apply(null, arr);
+            if( ret === Infinity ) return null;
+            return ret;
+        }
+    }
+}
+
+/**
+ * Elementの属性を一括登録する
+ * @param {Ojbect} obj 
+ */
+Element.prototype.setAttributes = function(obj) {
     for( let i of Object.entries(obj) ) {
         this.setAttribute(i[0], i[1]);
     }    
@@ -34,11 +110,11 @@ Element.prototype.setAttributes = (obj) => {
  * 自身が指定引数範囲内に含まれるか判定する
  * @param {Number} a 
  * @param {Number} b 
- * @param {Boolean} inclusive 
- * @returns 
+ * @param {Boolean} inclusive 引数自体を範囲に含むか
+ * @returns {Boolean} 自身が範囲内ならばtrue
  */
 Number.prototype.between = function(a, b, inclusive) {
-    let min = Math.min.apply(Math, [a, b]);
-    let max = Math.max.apply(Math, [a, b]);
+    let min = M424.Math.min(a, b);
+    let max = M424.Math.max(a, b);
     return inclusive ? min <= this && this <= max : min < this && this < max;
 }
